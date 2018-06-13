68051d1a724eda5c2a0f50071943c030f23e499c
knrdk <konrad1991@gmail.com>
2018-06-13 21:41:45 +0200

    running Aps.Net Core WebAPI on docker

--- AspNetCoreOnDocker.Api/Startup.cs
+++ AspNetCoreOnDocker.Api/Startup.cs
@@ -40,7 +40,7 @@ namespace AspNetCoreOnDocker.Api
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
